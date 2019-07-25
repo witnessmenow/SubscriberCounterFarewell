2a7d2f1f208a24d7e05e4521348d4efbfa2c89cb
witnessmenow <ecenuig@gmail.com>
2019-07-25 17:53:30 +0100

    PewDiePie Display

---
+++ PewDiePie/PewDiePie.ino
@@ -0,0 +1,418 @@
+/*******************************************************************
+   Showing PewDiePie's sub count on a 64x32 RGB LED Matrix
+
+   Has a brofist logo and can use the built in web interface to 
+   control the tilt.
+
+   Built using an ESP32 and using my own ESP32 Matrix Shield
+   https://www.tindie.com/products/brianlough/esp32-matrix-shield-mini-32/
+
+   Webserver servo code based on:
+   https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
+
+   By Brian Lough
+   YouTube: https://www.youtube.com/brianlough
+   Tindie: https://www.tindie.com/stores/brianlough/
+   Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+
+#include "image.h"
+
+// ----------------------------
+// Standard Libraries - Already Installed if you have ESP32 set up
+// ----------------------------
+
+#include <Ticker.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+#include <YoutubeApi.h>
+// Library for fetching the data from the YT API
+//
+// Install from the library manager
+// https://github.com/witnessmenow/arduino-youtube-api
+
+#include <ArduinoJson.h>
+// JSON parsing library required by the YT API library
+// NOTE: You must install 5.X, as the YT library doesnt
+// support V6
+//
+// Install from the library manager (use the drop down to change version)
+// https://github.com/bblanchon/ArduinoJson
+
+#define double_buffer // this must be enabled to stop flickering
+#include <PxMatrix.h>
+// The library for controlling the LED Matrix
+//
+// At time of writing this the double_buffer
+// Have been merged into the main PxMatrix library,
+// but have not been released, so you will need to install
+// from Github
+//
+// https://github.com/2dom/PxMatrix
+
+// Adafruit GFX library is a dependancy for the PxMatrix Library
+// Can be installed from the library manager
+// https://github.com/adafruit/Adafruit-GFX-Library
+
+#include <Servo.h>
+// Library for controlling Servos,
+// This is an ESP32 specific one that needs to be installed
+// 
+// It needs to be installed from Github
+// https://github.com/RoboticsBrno/ESP32-Arduino-Servo-Library
+
+
+
+// ----------------------------
+// Config - Replace These
+// ----------------------------
+
+char ssid[] = "SSID";       // your network SSID (name)
+char password[] = "password";  // your network key
+#define API_KEY "GOOGLE_API_KEY_GOES_HERE"  // your google apps API Token
+
+// PewDiePie's Channel ID
+#define CHANNEL_ID "UC-lHJZR3Gqxm24_Vd_AJ5Yw" // Had to use this to get it: https://commentpicker.com/youtube-channel-id.php
+
+// ----------------------------
+// Wiring and Display setup
+// ----------------------------
+
+#define P_LAT 22
+#define P_A 19
+#define P_B 23
+#define P_C 18
+#define P_D 5
+#define P_E 15
+#define P_OE 2
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+// GPIO the servo is attached to
+static const int servoPin = 26;
+
+// This defines the 'on' time of the display is us. The larger this number,
+// the brighter the display. If too large the ESP will crash
+uint8_t display_draw_time = 10; //10-50 is usually fine
+
+//PxMATRIX display(matrix_width, matrix_height, P_LAT, P_OE, P_A, P_B, P_C);
+//PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+PxMATRIX display(64, 32, P_LAT, P_OE, P_A, P_B, P_C, P_D, P_E);
+
+// Some standard colors
+uint16_t myRED = display.color565(255, 0, 0);
+uint16_t myGREEN = display.color565(0, 255, 0);
+uint16_t myBLUE = display.color565(0, 0, 255);
+uint16_t myWHITE = display.color565(255, 255, 255);
+uint16_t myYELLOW = display.color565(255, 255, 0);
+uint16_t myCYAN = display.color565(0, 255, 255);
+uint16_t myMAGENTA = display.color565(255, 0, 255);
+uint16_t myBLACK = display.color565(0, 0, 0);
+
+Ticker animation_ticker;
+bool finishedAnimating = false;
+unsigned long resetAnimationDue = 0;
+
+#define MAX_SERVO_ANGLE 150
+#define MIN_SERVO_ANGLE 90
+
+#define SERVO_START 90
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Decode HTTP GET value
+String valueString = String(5);
+int pos1 = 0;
+int pos2 = 0;
+
+WiFiClientSecure secureClient;
+YoutubeApi api(API_KEY, secureClient);
+
+String subCount = "";
+
+unsigned long timeBetweenRequests = 60000; // 1 minute
+unsigned long apiRequestDue = 0; // When API request is next Due
+
+void IRAM_ATTR display_updater() {
+  display.display(display_draw_time);
+}
+
+
+void display_update_enable(bool is_enable)
+{
+  if (is_enable)
+  {
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &display_updater, true);
+    timerAlarmWrite(timer, 2000, true);
+    timerAlarmEnable(timer);
+  }
+  else
+  {
+    timerDetachInterrupt(timer);
+    timerAlarmDisable(timer);
+  }
+}
+
+// Will center the given text
+void displayText(String text, int yPos) {
+  int16_t  x1, y1;
+  uint16_t w, h;
+  display.setTextSize(2);
+  char charBuf[text.length() + 1];
+  text.toCharArray(charBuf, text.length() + 1);
+  display.setTextSize(1);
+  display.getTextBounds(charBuf, 0, yPos, &x1, &y1, &w, &h);
+  int startingX = 33 - (w / 2);
+  display.setTextSize(1);
+  display.setCursor(startingX, yPos);
+  Serial.println(startingX);
+  Serial.println(yPos);
+  display.print(text);
+}
+
+void drawImage(int x, int y)
+{
+  int imageHeight = 24;
+  int imageWidth = 24;
+  int counter = 0;
+  for (int yy = 0; yy < imageHeight; yy++)
+  {
+    for (int xx = 0; xx < imageWidth; xx++)
+    {
+      display.drawPixel(xx + x , yy + y, broFist[counter]);
+      counter++;
+    }
+  }
+}
+
+void drawStuff()
+{
+  // Not clearing the display and redrawing it when you
+  // dont need to improves how the refresh rate appears
+  if (!finishedAnimating) {
+
+    // Step 1: Clear the display
+    display.clearDisplay();
+
+    // Step 2: Draw the SooubsImage
+    drawImage(20, 0);
+
+    // Step 3: Draw The Sub Count
+    display.setTextColor(myRED);
+    displayText(subCount, 24);
+
+    // Step 4: Display the buffer
+    display.showBuffer();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+
+  myservo.attach(servoPin);  // attaches the servo on the servoPin to the servo object
+  myservo.write(SERVO_START);
+  valueString = SERVO_START;
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  IPAddress ipAddress = WiFi.localIP();
+  Serial.println(ipAddress);
+
+  server.begin();
+
+
+  // Define your display layout here, e.g. 1/8 step
+  display.begin(16);
+
+  // Define your scan pattern here {LINE, ZIGZAG, ZAGGIZ, WZAGZIG, VZAG} (default is LINE)
+  //display.setScanPattern(LINE);
+
+  // Define multiplex implemention here {BINARY, STRAIGHT} (default is BINARY)
+  //display.setMuxPattern(BINARY);
+
+  display.setFastUpdate(true);
+  display.clearDisplay();
+  display.setTextColor(myCYAN);
+  display.setCursor(2, 0);
+  display.print("Servo");
+  display.setCursor(2, 8);
+  display.print("Control");
+  display.setTextColor(myMAGENTA);
+  display.setCursor(2, 16);
+  display.print(String(ipAddress[3]));
+  display_update_enable(true);
+  display.showBuffer();
+
+  delay(3000);
+}
+
+void moveServoSafe(int target)
+{
+  if (target <= MAX_SERVO_ANGLE && target >= MIN_SERVO_ANGLE) {
+    myservo.write(target);
+    Serial.print("Set servo to: ");
+  } else {
+    Serial.print("target out of bounds. Target: ");
+
+  }
+
+  Serial.println(target);
+}
+
+String getCommas(String number) {
+  int commaCount = (number.length() - 1) / 3;
+  String commasubCount = "";
+  commasubCount.reserve(50);
+  int commaOffsetIndex = 0;
+  for (int j = 0; j < commaCount; j++) {
+    commaOffsetIndex = number.length() - 3;
+    commasubCount = "," + number.substring(commaOffsetIndex) + commasubCount;
+    number.remove(commaOffsetIndex);
+  }
+
+  commasubCount = number + commasubCount;
+  
+  return commasubCount;
+}
+
+void handleWebClient(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+            client.println(".slider { width: 300px; }</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+
+            // Web Page
+            client.println("</head><body><h1>ESP32 with Servo</h1>");
+            client.println("<p>Position: <span id=\"servoPos\"></span></p>");
+
+            client.print("<input type=\"range\" min=\"");
+            client.print(MIN_SERVO_ANGLE);
+            client.print("\" max=\"");
+            client.print(MAX_SERVO_ANGLE);
+            client.println("\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\"" + valueString + "\"/>");
+
+            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
+            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
+            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+
+            client.println("</body></html>");
+
+            //GET /?value=180& HTTP/1.1
+            if (header.indexOf("GET /?value=") >= 0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              valueString = header.substring(pos1 + 1, pos2);
+
+              //Rotate the servo
+              moveServoSafe(valueString.toInt());
+            }
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+
+void handleYouTubeRequests(){
+  if (millis() > apiRequestDue)  {
+    if (api.getChannelStatistics(CHANNEL_ID))
+    {
+      Serial.println("---------Stats---------");
+      Serial.print("Subscriber Count: ");
+      Serial.println(api.channelStats.subscriberCount);
+      Serial.print("View Count: ");
+      Serial.println(api.channelStats.viewCount);
+      Serial.print("Comment Count: ");
+      Serial.println(api.channelStats.commentCount);
+      Serial.print("Video Count: ");
+      Serial.println(api.channelStats.videoCount);
+      // Probably not needed :)
+      //Serial.print("hiddenSubscriberCount: ");
+      //Serial.println(api.channelStats.hiddenSubscriberCount);
+      Serial.println("------------------------");
+
+      if(api.channelStats.subscriberCount < 100000000){
+        subCount = getCommas(String(api.channelStats.subscriberCount));
+      } else {
+        // 100,000,000 + will not fit on screen with commas
+        subCount = String(api.channelStats.subscriberCount);
+      }
+      drawStuff();
+    } else {
+      Serial.println("YT data fetch failed");
+    }
+    apiRequestDue = millis() + timeBetweenRequests;
+  }
+}
+
+
+void loop() {
+  handleWebClient();
+  handleYouTubeRequests();
+}
